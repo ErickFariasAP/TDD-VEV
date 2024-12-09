ccbdd8a0beb4c8c410459a5261de1974a8ac8239
Erick Farias de Almeida Pereira <105391685+ErickFariasAP@users.noreply.github.com>
2024-12-08 21:53:21 -0300

    setup inicial do projeto

---
+++ src/ProcessadorContas/test/test/java/processadorContasTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class processadorContasTest {
+
+    @Test
+    public void verificarJunit() {
+        Assertions.assertTrue(true);
+    }
+}
